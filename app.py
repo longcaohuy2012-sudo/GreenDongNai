9d071e5490be96c930fe7777eb2d9d2c94854519
longcaohuy2012-sudo <longcaohuy2012@gmail.com>
2026-03-27 13:39:00 +0000

    Evening ok

--- app.py
+++ app.py
@@ -191,5 +191,5 @@ def logout():
     return redirect(url_for('landing'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(host='0.0.0.0', port=port)
+        port = int(os.environ.get("PORT", 5000)) 
+            app.run(host='0.0.0.0', port=port)
